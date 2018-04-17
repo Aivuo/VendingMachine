0ba05a74ab3733797698771afab6ed6f5d50ca2d
Aivuo <rioneda1@gmail.com>
2018-04-17 13:05:50 +0200

    You can now add money

---
+++ VendingMachine/VendingMachine/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    abstract class Product
+    {
+        protected string name = null;
+        protected int price = 0;
+
+        public virtual void Buy()
+        {
+
+        }
+
+        public virtual void Examine()
+        {
+
+        }
+
+        public virtual void Use()
+        {
+
+        }
+    }
+}
--- VendingMachine/VendingMachine/Program.cs
+++ VendingMachine/VendingMachine/Program.cs
@@ -10,6 +10,73 @@ namespace VendingMachine
     {
         static void Main(string[] args)
         {
+            int money = 0;
+
+            Menu(money);
+        }
+
+        private static void Menu(int money)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\tMoney: {0:N0}sek" +
+                                "\n\t1: Add money" +
+                                "\n\t2: Buy something" +
+                                "\n\t3: Leave", money);
+
+                var input = Console.ReadKey();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        money = AddMoney(money);
+
+                        break;
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+
+                        break;
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        return;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static int AddMoney(int money)
+        {
+            int[] valueDenomination = { 5, 10, 20, 50, 100, 200, 500, 1000 };
+            int choice = 0;
+            int i = valueDenomination.Count() - 1;
+
+            Console.WriteLine(String.Format("\t\tPlease add money to the machine" +
+                                            "\n\t\tYou can add any valid Swedish currency except 1Sek coins"));
+
+            int.TryParse(Console.ReadLine(), out choice);
+
+            while (choice >= valueDenomination[0])
+            {
+                if (choice >= valueDenomination[i])
+                {
+                    money += valueDenomination[i];
+                    choice -= valueDenomination[i];
+                }
+                if (choice < valueDenomination[i])
+                {
+                    i--;
+                }
+                Console.WriteLine(money + " " + i);
+            }
+            Console.WriteLine("Here is your change back: {0}sek", choice);
+            Console.ReadKey();
+            return money;
+
+
+
         }
     }
 }
